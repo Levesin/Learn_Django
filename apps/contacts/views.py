44626728a6b4258554d4b16df40a285980ca22d8
levesin <lionrav19@gmail.com>
2024-02-05 18:07:07 +0200

    hw3 add CRUD contacts

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 from apps.contacts.models import Contact
 
 
@@ -11,3 +12,33 @@ def list_contacts(request):
             "contacts": Contact.objects.all(),
         },
     )
+
+
+class ContactCreateView(CreateView):
+    model = Contact
+    fields = (
+        "name",
+        "phone",
+    )
+    success_url = reverse_lazy("contacts:list")
+
+
+class ContactUpdateView(UpdateView):
+    model = Contact
+    fields = (
+        "id",
+        "name",
+        "phone",
+    )
+    success_url = reverse_lazy("contacts:list")
+
+
+class ContactDeleteView(DeleteView):
+    model = Contact
+    template_name = "contacts/contacts_confirm_delete.html"
+    success_url = reverse_lazy("contacts:list")
+
+
+class ContactDetailView(DetailView):
+    model = Contact
+    pk_url_kwarg = "pk"
